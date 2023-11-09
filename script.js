5eba0a2d6245c83ce64c55a0e5a836fb3c64339d
Henrique Heron <henriqueheron1@gmail.com>
2023-11-09 17:35:05 -0300

    feat: make all buttons fit within the container

---
+++ script.js
@@ -0,0 +1,33 @@
+document.addEventListener("DOMContentLoaded", () => {
+
+    const container = document.querySelector('.container');
+    const gridSizeBtn = document.querySelector('#grid-size-button');
+
+    gridSizeBtn.addEventListener('click', () => {
+        const squares = document.querySelectorAll('.square');
+        squares.forEach(square => {
+            square.remove();
+        })
+
+
+        let gridSize = prompt("Select your grid size: ", 7);
+        if (gridSize > 100) {
+            while (gridSize > 100) {
+                // gridSize = 0;
+                gridSize = prompt("Cannot make grids over 100x100 squares in size. Select your grid size again: ", 7);
+            }
+        }
+
+        container.style.gridTemplateColumns = `repeat(${gridSize}, 1fr`;
+        container.style.gridTemplateRows = `repeat(${gridSize}, 1fr`;
+
+        for (let i = 1; i <= gridSize * gridSize; i++) {
+            const square = document.createElement('div');
+            square.classList.add('square');
+            square.textContent = i;
+            container.appendChild(square);
+        }
+    })
+
+
+})
