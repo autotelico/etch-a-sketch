d14d3d6af12df40778484c00e33b7b62f1628236
Henrique Heron <henriqueheron1@gmail.com>
2023-11-09 17:54:30 -0300

    fix: make it an actual etch-a-sketch by adding mouseover effects to it

--- script.js
+++ script.js
@@ -11,22 +11,26 @@ document.addEventListener("DOMContentLoaded", () => {
 
 
         let gridSize = prompt("Select your grid size: ", 7);
-        if (gridSize > 100) {
-            while (gridSize > 100) {
-                // gridSize = 0;
-                gridSize = prompt("Cannot make grids over 100x100 squares in size. Select your grid size again: ", 7);
-            }
+
+        while (gridSize > 200 || gridSize < 25) {
+            gridSize = prompt("Cannot make grids smaller than 25x25 or bigger than 200x200 squares in size. Select your grid size again: ", 7);
         }
+    
 
         container.style.gridTemplateColumns = `repeat(${gridSize}, 1fr`;
-        container.style.gridTemplateRows = `repeat(${gridSize}, 1fr`;
+    container.style.gridTemplateRows = `repeat(${gridSize}, 1fr`;
 
-        for (let i = 1; i <= gridSize * gridSize; i++) {
-            const square = document.createElement('div');
-            square.classList.add('square');
-            container.appendChild(square);
-        }
-    })
+    for (let i = 1; i <= gridSize * gridSize; i++) {
+        const square = document.createElement('div');
+        square.classList.add('square');
+        container.appendChild(square);
+        square.addEventListener('mouseover', () => {
+            square.style.backgroundColor = 'purple';
+        })
+    }
+})
+
+    
 
 
 })
